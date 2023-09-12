f5c01b86ebe210cf429e466d1e3dbc29f27b14a1
Bruno Krugel <brunokrugel@gmail.com>
2023-09-12 11:57:53 -0300

    add highlights for extended integrations

--- lua/nvcommunity/diagnostics/trouble/init.lua
+++ lua/nvcommunity/diagnostics/trouble/init.lua
@@ -1,8 +1,23 @@
 ---@type NvPluginSpec
 local spec = {
   "folke/trouble.nvim",
-  cmd = {"Trouble", "TroubleToggle"},
-  config = true, -- Todo: change when base46 has colors? for this
+  cmd = { "Trouble", "TroubleToggle", "TodoTrouble" },
+  opts = {},
+  init = function()
+    local ext = require("custom.chadrc").ui.extended_integrations or {}
+    table.insert(ext, "trouble")
+    require("custom.chadrc").ui.extended_integrations = ext
+    require("base46").load_all_highlights()
+    dofile(vim.g.base46_cache .. "trouble")
+    require("core.utils").load_mappings "todo"
+  end,
+}
+
+require("core.mappings").todo = {
+  n = {
+    ["<leader>t"] = { "<CMD>TroubleToggle<CR>", "Toggle diagnostics" },
+    ["<leader>td"] = { "<CMD>TodoTrouble keywords=TODO,FIX,FIXME,BUG,TEST,NOTE<CR>", "Todo/Fix/Fixme" },
+  },
 }
 
 return spec
--- lua/nvcommunity/editor/rainbowdelimiters/init.lua
+++ lua/nvcommunity/editor/rainbowdelimiters/init.lua
@@ -3,6 +3,13 @@ local spec = {
     {
       "hiphish/rainbow-delimiters.nvim",
       event = "BufReadPost",
+      init = function()
+        local ext = require("custom.chadrc").ui.extended_integrations or {}
+        table.insert(ext, "rainbowdelimiters")
+        require("custom.chadrc").ui.extended_integrations = ext
+        require("base46").load_all_highlights()
+        dofile(vim.g.base46_cache .. "rainbowdelimiters")
+      end,
       config = function()
         local rainbow_delimiters = require "rainbow-delimiters"
   
