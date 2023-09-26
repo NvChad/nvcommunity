81d865cb875ef9e5f37e1ce05d4cbe6667a8a7a7
ywwa (Gabriels) <gabripub@gmail.com>
2023-09-26 15:53:08 +0300

    fix spaces, config->opts fn{}

--- lua/nvcommunity/folds/ufo/init.lua
+++ lua/nvcommunity/folds/ufo/init.lua
@@ -1,82 +1,82 @@
 ---@type NvPluginSpec
 local spec = {
-	{
-		"kevinhwang91/nvim-ufo",
-		event = "VimEnter",
-		dependencies = {
-			"kevinhwang91/promise-async",
-			{
-				"luukvbaal/statuscol.nvim",
-				config = function()
+  {
+    "kevinhwang91/nvim-ufo",
+    event = "VimEnter",
+    dependencies = {
+      "kevinhwang91/promise-async",
+      {
+        "luukvbaal/statuscol.nvim",
+        opts = function()
           local builtin = require "statuscol.builtin"
-					require("statuscol").setup({
-						relculright = true,
-						bt_ignore = { "nofile", "prompt", "terminal", "packer" },
-						ft_ignore = {
-							"NvimTree",
-							"dashboard",
-							"nvcheatsheet",
-							"dapui_watches",
-							"dap-repl",
-							"dapui_console",
-							"dapui_stacks",
-							"dapui_breakpoints",
-							"dapui_scopes",
-							"help",
-							"vim",
-							"alpha",
-							"dashboard",
-							"neo-tree",
-							"Trouble",
-							"noice",
-							"lazy",
-							"toggleterm",
-						},
-						segments = {
-							-- Segment: Add padding
-							{
-								text = { " " },
-							},
-							-- Segment: Fold Column
-							{ text = { builtin.foldfunc }, click = "v:lua.ScFa" },
-							-- Segment: Add padding
-							{
-								text = { " " },
-							},
-							-- Segment : Show signs with one character width
-							{
-								sign = {
-									name = { ".*" },
-									maxwidth = 1,
-									colwidth = 1,
-								},
-								auto = true,
-								click = "v:lua.ScSa",
-							},
-							-- Segment: Show line number
-							{
-								text = { " ", " ", builtin.lnumfunc, " " },
-								click = "v:lua.ScLa",
-								condition = { true, builtin.not_empty },
-							},
-							-- Segment: Add padding
-							{
-								text = { " " },
-								hl = "Normal",
-								condition = { true, builtin.not_empty },
-							},
-						},
-					})
-				end,
-			},
-		},
-		opts = {
-			close_fold_kinds = { "imports" },
-			provider_selector = function()
-				return { "treesitter", "indent" }
-			end,
-		},
-	},
+          return {
+            relculright = true,
+            bt_ignore = { "nofile", "prompt", "terminal", "packer" },
+            ft_ignore = {
+              "NvimTree",
+              "dashboard",
+              "nvcheatsheet",
+              "dapui_watches",
+              "dap-repl",
+              "dapui_console",
+              "dapui_stacks",
+              "dapui_breakpoints",
+              "dapui_scopes",
+              "help",
+              "vim",
+              "alpha",
+              "dashboard",
+              "neo-tree",
+              "Trouble",
+              "noice",
+              "lazy",
+              "toggleterm",
+            },
+            segments = {
+              -- Segment: Add padding
+              {
+                text = { " " },
+              },
+              -- Segment: Fold Column
+              { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+              -- Segment: Add padding
+              {
+                text = { " " },
+              },
+              -- Segment : Show signs with one character width
+              {
+                sign = {
+                  name = { ".*" },
+                  maxwidth = 1,
+                  colwidth = 1,
+                },
+                auto = true,
+                click = "v:lua.ScSa",
+              },
+              -- Segment: Show line number
+              {
+                text = { " ", " ", builtin.lnumfunc, " " },
+                click = "v:lua.ScLa",
+                condition = { true, builtin.not_empty },
+              },
+              -- Segment: Add padding
+              {
+                text = { " " },
+                hl = "Normal",
+                condition = { true, builtin.not_empty },
+              },
+            },
+          }
+        end,
+      },
+    },
+    opts = {
+      close_fold_kinds = { "imports" },
+      provider_selector = function()
+        return { "treesitter", "indent" }
+      end,
+    },
+  },
 }
 
 return spec
