d7419914f87afb527905c8b1bd50b041675da844
ambaradan <87865413+ambaradan@users.noreply.github.com>
2023-10-14 12:21:34 +0200

    add missing dependency

--- lua/nvcommunity/git/neogit/init.lua
+++ lua/nvcommunity/git/neogit/init.lua
@@ -4,7 +4,10 @@ local spec = {
         "NeogitOrg/neogit",
         ft = { "diff" },
         cmd = "Neogit",
-        dependencies = {"sindrets/diffview.nvim"},
+        dependencies = {
+            "sindrets/diffview.nvim",
+            "nvim-lua/plenary.nvim",
+        },
         opts = {
             signs = {section = {"", ""}, item = {"", ""}},
             disable_commit_confirmation = true,
