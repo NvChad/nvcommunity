e86533146c207d355ecd43e72efbe9624ab45ad1
akane <akane@maidagency.org>
2024-01-20 08:41:47 +0800

    add cmp source for conjure plus readme update

--- lua/nvcommunity/tools/conjure/init.lua
+++ lua/nvcommunity/tools/conjure/init.lua
@@ -1,26 +1,36 @@
 ---@type NvPluginSpec
 local spec = {
   {
-    "Olical/conjure",
-    ft = {
-      "clojure",
-      "fennel",
-      "janet",
-      "hy",
-      "julia",
-      "racket",
-      "scheme",
-      "lua",
-      "lisp",
-      "python",
-      "rust",
-      "sql",
-    },
-    opts = {},
-    config = function()
-      require("conjure.main").main()
-      require("conjure.mapping")["on-filetype"]()
+    "hrsh7th/nvim-cmp",
+    config = function(_, opts)
+      table.insert(opts.sources, { name = "conjure" })
+      require("cmp").setup(opts)
     end,
+    dependencies = {
+      {
+        "Olical/conjure",
+        ft = {
+          "clojure",
+          "fennel",
+          "janet",
+          "hy",
+          "julia",
+          "racket",
+          "scheme",
+          "lua",
+          "lisp",
+          "python",
+          "rust",
+          "sql",
+        },
+        opts = {},
+        config = function()
+          require("conjure.main").main()
+          require("conjure.mapping")["on-filetype"]()
+        end,
+        dependencies = { "PaterJason/cmp-conjure" },
+      },
+    },
   },
 }
 
--- lua/nvcommunity/tools/conjure/readme.md
+++ lua/nvcommunity/tools/conjure/readme.md
@@ -1,19 +1,41 @@
-# Conjure
+# [Conjure](https://github.com/Olical/conjure)
 
 Interactive REPL-based development plugin for Neovim.
 
 Make sure to set localleader.
+
 ```lua
-vim.g.maplocalleader = "m"
--- set localleader as m
+vim.g.maplocalleader = ","
+-- set localleader to comma key
 ```
 
 Example configuration:
+
 ```lua
 {
     "conjure",
     init = function()
       vim.g["conjure#log#hud#enabled"] = false
+      -- add more here
+    end,
+},
+```
+
+# Add additional cmp source
+
+Supposed you have multiple cmp sources.
+For example, Codeium from Nvcommunity.
+
+Place this snippet at the bottom of plugins table.
+
+```lua
+{
+    "hrsh7th/nvim-cmp",
+    config = function(_, opts)
+      table.insert(opts.sources, { name = "conjure" })
+      table.insert(opts.sources, { name = "codeium" })
+      -- add more sources as example
+      require("cmp").setup(opts)
     end,
 },
 ```
