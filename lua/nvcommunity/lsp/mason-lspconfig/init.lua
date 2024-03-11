1d96504675c7514a9e89c6bdb14921cc8f7d10ca
magency-prod <151979723+magency-prod@users.noreply.github.com>
2024-03-11 14:10:38 +0800

    fix issue #29 (#30)

--- lua/nvcommunity/lsp/mason-lspconfig/init.lua
+++ lua/nvcommunity/lsp/mason-lspconfig/init.lua
@@ -1,6 +1,6 @@
 local setup = function(_, opts)
-  local on_attach = require("plugins.configs.lspconfig").on_attach
-  local capabilities = require("plugins.configs.lspconfig").capabilities
+  local on_attach = require("nvchad.configs.lspconfig").on_attach
+  local capabilities = require("nvchad.configs.lspconfig").capabilities
 
   local lspconfig = require "lspconfig"
 
