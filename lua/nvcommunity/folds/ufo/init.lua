936e9a145cfeffb4a2f8d180fda3a8ee307058ba
Mark <bitterjug@gmail.com>
2024-03-15 17:00:59 +0000

    Fix redundant ufo option
    close_fold_kinds has been deprecated in favour of
    close_fold_kinds_for_ft. Here I update the format
    keeping the same default value.

--- lua/nvcommunity/folds/ufo/init.lua
+++ lua/nvcommunity/folds/ufo/init.lua
@@ -104,7 +104,7 @@ local spec = {
       },
     },
     opts = {
-      close_fold_kinds = { "imports" },
+      close_fold_kinds_for_ft = { default = { "imports" } },
       provider_selector = function()
         return { "treesitter", "indent" }
       end,
