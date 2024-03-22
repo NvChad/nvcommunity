62b304f0d3ab1826f604b29013ed6e0cffec2fae
Sidhanth Rathod <siduck@tutanota.com>
2024-03-22 08:05:09 +0530

    Avoid loading telescope module in init func
    init func in lazy runs after startup so use confi func instead

--- lua/nvcommunity/editor/telescope-undo/init.lua
+++ lua/nvcommunity/editor/telescope-undo/init.lua
@@ -4,9 +4,8 @@ local spec = {
   dependencies = {
     {
       "debugloop/telescope-undo.nvim",
-      init = function()
+      config = function()
         local map = vim.keymap.set
-
         map("n", "<leader>fu", "<CMD>Telescope undo<CR>", { desc = "Find undo" })
 
         require("telescope").load_extension "undo"
