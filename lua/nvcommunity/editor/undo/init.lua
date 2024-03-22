5c06cc531e5827965b33f9db8d813eb213ffbec7
siduck <siduck@tutanota.com>
2024-03-22 06:42:19 +0530

    rm un-needed init func for fundo
    undofile is already set in default options

--- lua/nvcommunity/editor/undo/init.lua
+++ lua/nvcommunity/editor/undo/init.lua
@@ -4,9 +4,6 @@ local spec = {
     "kevinhwang91/nvim-fundo",
     event = "VeryLazy",
     opts = {},
-    init = function()
-      vim.o.undofile = true
-    end,
     build = function()
       require("fundo").install()
     end,
