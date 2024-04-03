2611f7f644a10da07bdd42f4691ae8ce196bfce7
OutofRange <alwaysoutofrange@proton.me>
2024-04-03 17:35:14 +0200

    Update init.lua

--- lua/nvcommunity/editor/telescope-undo/init.lua
+++ lua/nvcommunity/editor/telescope-undo/init.lua
@@ -3,11 +3,12 @@ local spec = {
   dependencies = {
     {
       "debugloop/telescope-undo.nvim",
-      config = function()
+      init = function()
         local map = vim.keymap.set
 
         map("n", "<leader>fu", "<CMD>Telescope undo<CR>", { desc = "Find undo" })
-
+      end,
+      config = function()
         require("telescope").load_extension "undo"
       end,
     },
