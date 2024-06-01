92136b82c89bbcbfed570da3b311177d51464f96
Przemyslaw Pietras <przemyslawp94@gmail.com>
2024-06-01 12:33:18 +0200

    fix leap.nvim config

--- lua/nvcommunity/motion/leap/init.lua
+++ lua/nvcommunity/motion/leap/init.lua
@@ -13,8 +13,6 @@ local spec = {
         leap.opts[k] = v
       end
       leap.add_default_mappings(true)
-      vim.keymap.del({ "x", "o" }, "x")
-      vim.keymap.del({ "x", "o" }, "X")
     end,
   },
 }
