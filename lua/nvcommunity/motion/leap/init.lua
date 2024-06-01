ff9c80f236233c51f035312567224b0658574a9f
Przemysław Pietras <przemyslawp94@gmail.com>
2024-06-01 12:38:36 +0200

    Add leap.nvim plugin (#22)
    * add leap.nvim plugin
    
    * fix leap.nvim config

---
+++ lua/nvcommunity/motion/leap/init.lua
@@ -0,0 +1,20 @@
+---@type NvPluginSpec
+local spec = {
+  {
+    "ggandor/leap.nvim",
+    keys = {
+      { "s",  mode = { "n", "x", "o" }, desc = "Leap forward to" },
+      { "S",  mode = { "n", "x", "o" }, desc = "Leap backward to" },
+      { "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
+    },
+    config = function(_, opts)
+      local leap = require "leap"
+      for k, v in pairs(opts) do
+        leap.opts[k] = v
+      end
+      leap.add_default_mappings(true)
+    end,
+  },
+}
+
+return spec
