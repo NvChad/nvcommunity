102e101d651c2a50eaba758c1241da88d9840198
f40 <151979723+magency-prod@users.noreply.github.com>
2024-06-01 17:59:12 +0800

    update trouble mappings (#33)

--- lua/nvcommunity/diagnostics/trouble/init.lua
+++ lua/nvcommunity/diagnostics/trouble/init.lua
@@ -12,7 +12,7 @@ local spec = {
   init = function()
     local map = vim.keymap.set
 
-    map("n", "<leader>t", "<CMD>TroubleToggle<CR>", { desc = "Toggle diagnostics" })
+    map("n", "<leader>t", "<CMD>Trouble diagnostics toggle<CR>", { desc = "Toggle diagnostics" })
     map("n", "<leader>td", "<CMD>TodoTrouble keywords=TODO,FIX,FIXME,BUG,TEST,NOTE<CR>", { desc = "Todo/Fix/Fixme" })
   end,
 }
