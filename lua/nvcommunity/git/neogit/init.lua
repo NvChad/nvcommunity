2131ee12046a448989edbe96225352dd96a01f28
Sidhanth Rathod <siduck@tutanota.com>
2024-06-02 14:19:31 +0530

    Update neogit config function with base46 highlights

--- lua/nvcommunity/git/neogit/init.lua
+++ lua/nvcommunity/git/neogit/init.lua
@@ -1,19 +1,21 @@
 ---@type NvPluginSpec
 local spec = {
-  {
-    "NeogitOrg/neogit",
-    ft = { "diff" },
-    cmd = "Neogit",
-    dependencies = {
-      "sindrets/diffview.nvim",
-      "nvim-lua/plenary.nvim",
-    },
-    opts = {
-      signs = { section = { "", "" }, item = { "", "" } },
-      disable_commit_confirmation = true,
-      integrations = { diffview = true },
-    },
+  "NeogitOrg/neogit",
+  cmd = "Neogit",
+  ft = { "diff" },
+  dependencies = {
+    "sindrets/diffview.nvim",
   },
+  opts = {
+    signs = { section = { "", "" }, item = { "", "" } },
+    disable_commit_confirmation = true,
+    integrations = { diffview = true },
+  },
+  config = function(_, opts)
+    require("neogit").setup(opts)
+    dofile(vim.g.base46_cache .. "git")
+    dofile(vim.g.base46_cache .. "neogit")
+  end,
 }
 
 return spec
